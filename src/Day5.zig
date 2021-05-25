86cec0aab380002e79ce19f1bf398296e2d7beed
Tristan Crawford <tristanthtcrawford@gmail.com>
2021-05-25 00:47:16 -0500

    Day 5 - Part Two - Complete

--- src/Day5.zig
+++ src/Day5.zig
@@ -4,16 +4,12 @@ const print = std.debug.print;
 pub fn main() !void {
     const input = @embedFile("data/Day5.txt");
 
-    // const input =
-    // \\BFFFBBFRRR
-    // \\FFFBBBFRRR
-    // \\BBFFBBFRLL
-    // ;
+    var highest: usize = 0;
+
+    var seat_idx: usize = 0;
+    var seat_ids = [_]usize{0} ** 814;
 
     var toks = std.mem.tokenize(input, "\n");
-
-    var highest: i32 = 0;
-
     while (toks.next()) |line| {
         var row_lower: usize = 0;
         var row_upper: usize = 127;
@@ -21,26 +17,19 @@ pub fn main() !void {
         var col_lower: usize = 0;
         var col_upper: usize = 7;
 
-        var last_row: u8 = undefined;
-        var last_col: u8 = undefined;
-
         for (line) |char| {
             switch (char) {
                 'F' => {
                     row_upper = @divFloor(row_upper - row_lower, 2) + row_lower;
-                    last_row = char;
                 },
                 'B' => {
                     row_lower = @divFloor(row_upper - row_lower, 2) + row_lower;
-                    last_row = char;
                 },
                 'L' => {
                     col_upper = @divFloor(col_upper - col_lower, 2) + col_lower;
-                    last_col = char;
                 },
                 'R' => {
                     col_lower = @divFloor(col_upper - col_lower, 2) + col_lower;
-                    last_col = char;
                 },
                 else => {},
             }
@@ -49,13 +38,25 @@ pub fn main() !void {
         const seat = row_upper * 8 + col_upper;
 
         if (seat > highest)
-            highest = @intCast(i32, seat);
+            highest = seat;
 
-        // print("{s}\n", .{line});
-        // print("Row: {d}..{d}\n", .{ row_lower, row_upper });
-        // print("Col: {d}..{d}\n", .{ col_lower, col_upper });
-        // print("Seat: {d}\n", .{seat});
+        seat_ids[seat_idx] = seat;
+        seat_idx += 1;
+    }
+
+    std.sort.sort(usize, &seat_ids, {}, comptime std.sort.asc(usize));
+
+    var missing: usize = undefined;
+    var prev_id: usize = seat_ids[0];
+
+    for (seat_ids) |id| {
+        const diff = id - prev_id;
+        if (diff > 1) {
+            missing = id - 1;
+        }
+        prev_id = id;
     }
 
     print("Highest: {d}\n", .{highest});
+    print("Missing: {d}\n", .{missing});
 }
